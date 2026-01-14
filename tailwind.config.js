cb85adfc1bcbeb1dae6d696c1a8e686a91617e1a
garyeung <5206130+garyeung@users.noreply.github.com>
2026-01-14 14:20:18 +0800

    adjust theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,28 @@ export default {
   theme: {
     extend: {
       colors:{
-        "BLUE-900": '#0B0D17',
-        "BLUE-300": '#D0D6F9',
+        "blue-900": '#0B0D17',
+        "blue-300": '#D0D6F9',
         "LOGO-LINE": '#979797',
+        "white": "#FFFFFF",
 
       },
-      fontFamily: {
-        sans:["Barlow Condensed", "sans-serif"],
-        serif: ["Bellefair", "serif"],
+      spacing: {
+        "25": "2px",
+        "50": "4px",
+        "100": "8px",
+        "150": "12px",
+        "200": "16px",
+        "300": "24px",
+        "400": "32px",
+        "500": "40px",
+        "600": "48px",
+        "800": "64px",
+        "1000": "80px",
+        "1200": "96px",
+        "1600": "128px"
       }
+
     },
   },
   plugins: [],
