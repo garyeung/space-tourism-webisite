7dce5d14c0f7cba917b2212db4d4093c641207f6
garyeung <5206130+garyeung@users.noreply.github.com>
2026-01-14 17:56:18 +0800

    refactoring Logo component

---
+++ src/components/bases/Logo.tsx
@@ -0,0 +1,10 @@
+import IconLogo from "@/assets/shared/logo.svg?react"
+const Logo = () => {
+    return (
+        <div>
+            <IconLogo title="logo"/>
+        </div>
+    )
+}
+
+export default Logo;
