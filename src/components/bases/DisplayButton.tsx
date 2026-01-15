ea3fa230ba8fc492b5d6f4e68c9e6d867c583f66
garyeung <5206130+garyeung@users.noreply.github.com>
2026-01-15 12:05:37 +0800

    create display button

---
+++ src/components/bases/DisplayButton.tsx
@@ -0,0 +1,38 @@
+import { NavLink } from "react-router-dom"
+
+interface Props {
+    path: string,
+}
+
+const DisplayButton = ({path}: Props) => {
+
+    return (
+        <NavLink to={path}
+        className={`
+            w-36
+            h-36
+            md:w-[272px]
+            md:h-[272px]
+            rounded-full
+            bg-white
+            text-blue-900 
+            text-center
+            text-lg
+            uppercase
+            md:text-[32px]
+            font-bellefair
+            hover:text-opacity-50
+            outline-[88px]
+            outline-transparent
+            outline-offset-0
+            transition-all
+            duration-500
+            hover:outline-[rgba(255,255,255,0.1)]
+            `}
+        >
+            explore
+        </NavLink>
+    )
+}
+
+export default DisplayButton; 
