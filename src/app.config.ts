8c1a757e4e2f23f051d8e445b64524dd79208165
garyeung <5206130+garyeung@users.noreply.github.com>
2026-01-16 21:27:47 +0800

    app configuration file

---
+++ src/app.config.ts
@@ -0,0 +1,16 @@
+interface HomeConfig {
+    subtitle: string,
+    title: string,
+    description: string
+}
+
+export interface AppConfig {
+    home: HomeConfig
+}
+export const appConfig = {
+    home: {
+        subtitle: "so, you want to travel to",
+        title: "space",
+        description: "Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!" 
+    }
+}
