9a70902d8d8c53de35207d11b3899fc952e6276c
garyeung <5206130+garyeung@users.noreply.github.com>
2026-01-16 11:32:02 +0800

    adjust Navigation component

--- src/components/combinations/Navigation.tsx
+++ src/components/combinations/Navigation.tsx
@@ -2,49 +2,37 @@ import { useState } from "react"
 import Logo from "@/components/bases/Logo"
 import IconHambuger from "@/assets/shared/icon-hamburger.svg?react"
 import IconClose from "@/assets/shared/icon-close.svg?react"
-import Nav, { NavProps } from "@/components/bases/Nav"
+import NavLinks from "@/components/combinations/NavLinks"
 
-export interface NavigationProps {
-    routes: NavProps[]
-}
-
-const Navigation = ({routes}: NavigationProps) => {
+const Navigation = () => {
     const [open, setOpen] = useState(false);
-    const handleOpen = () => {
-        setOpen(true);
-    }
-    const handleClose = () => {
-        setOpen(false);
-    }
 
-    const navs = routes.map((route, index) => {
-        const pre = index.toFixed(2);
-        const label = pre + " " +route.label;
-        return <Nav 
-        key={label} 
-        path={route.path} 
-        label={label}
-        />
-    })
+    const toggleMenu = () => setOpen((pre) => !pre);
 
     return (
         <nav>
             <div>
                 <Logo/>
             </div>
-            <div className="md:hidden">
+            <div className="md:hidden" id="mobile-menu">
                 <button 
-                onClick={handleOpen}
-                aria-label="open menu"><IconHambuger /></button>
-                <div className={`${open? "block": "hidden"}`}>
-                    <button onClick={handleClose} aria-label="close menu"><IconClose /></button>
+                onClick={toggleMenu}
+                aria-label="Toggle menu"
+                aria-expanded={open}
+                aria-controls="mobile-menu"
+                ><IconHambuger /></button>
+                <div className={`${open? "block fixed z-10": "hidden"}`}>
+                    <button onClick={toggleMenu} aria-label="Toggle menu"
+                    aria-expanded={open}
+                    aria-controls="mobile-menu"
+                    ><IconClose /></button>
                     <div>
-                        {navs}
+                        <NavLinks/> 
                     </div>
                 </div>
             </div>
             <div className="hidden md:block">
-                {navs}
+                <NavLinks/>
             </div>
         </nav>
     )
