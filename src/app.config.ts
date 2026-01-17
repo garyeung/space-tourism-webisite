4adf3fdf4580b22a70e02ed8884b58a715f4414b
garyeung <5206130+garyeung@users.noreply.github.com>
2026-01-17 10:14:08 +0800

    add backgrounds

--- src/app.config.ts
+++ src/app.config.ts
@@ -2,7 +2,17 @@ interface HomeConfig {
     subtitle: string,
     title: string,
     description: string
+    background: {
+        mobile: string,
+        desktop: string,
+        tablet: string
+    } 
 }
+
+import homebackgroundMobile from "@/assets/home/background-home-mobile.jpg"
+import homebackgroundDesktop from "@/assets/home/background-home-desktop.jpg"
+import homebackgroundTablet from "@/assets/home/background-home-tablet.jpg"
+
 
 export interface AppConfig {
     home: HomeConfig
@@ -11,6 +21,11 @@ export const appConfig = {
     home: {
         subtitle: "so, you want to travel to",
         title: "space",
-        description: "Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!" 
+        description: "Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!" ,
+        background: {
+            mobile: homebackgroundMobile,
+            desktop: homebackgroundDesktop,
+            tablet: homebackgroundTablet 
+        } 
     }
 }
