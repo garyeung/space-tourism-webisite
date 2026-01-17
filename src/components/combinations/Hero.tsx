c567d940127f6edfb7c67454862bb926d7ef44de
garyeung <5206130+garyeung@users.noreply.github.com>
2026-01-17 16:38:32 +0800

    adjust style

--- src/components/combinations/Hero.tsx
+++ src/components/combinations/Hero.tsx
@@ -9,7 +9,7 @@ const Hero = (props:HeroProps) => {
     return (
         <section className="grid grid-rows-[auto,1fr]">
             <article className="text-blue-300 text-center">
-              <p className="font-barlow text-base uppercase">{props.subtitle}</p>
+              <p className="font-barlow text-base uppercase tracking-[15%]">{props.subtitle}</p>
               <h1 className="
                 font-bellefair
               text-white
