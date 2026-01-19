4e0b1e737093c6a3b8ec03a3c98fb2965128c7df
garyeung <5206130+garyeung@users.noreply.github.com>
2026-01-19 14:56:42 +0800

    import page layout component

--- src/pages/Destination.tsx
+++ src/pages/Destination.tsx
@@ -1,4 +1,5 @@
 import Line from "@/components/bases/Line";
+import PageLayout from "@/components/bases/PageLayout";
 import ReactiveImg from "@/components/bases/ReactiveImg";
 import Statistic from "@/components/bases/Statistic";
 import Tab from "@/components/bases/Tab";
@@ -30,55 +31,48 @@ const Destination = ({destinations}:Props) => {
     }
 
  
-
     return (
-        <section className="flex flex-col gap-y-6">
+        <PageLayout 
+        number={1}
+        title={"pick your destination"}
+        explantion= {
+          <div className="flex flex-col gap-y-6">
             <div>
-                <h2 className="font-barlow text-base
-                text-center
-                uppercase
-                tracking-[15%]
-               text-white"
-                ><span className="opacity-25 pr-6">01</span> pick your destination
-                </h2>
+                <ul className="flex flex-row gap-x-8 justify-center">
+                    {
+                        destinations.map((item) => {
+                            return (
+                                <li key={item.name}>
+                                    <Tab label={item.name} handleClick={() => handleTab(item.name)} 
+                                    active={destination.name === item.name}
+                                        />
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
             </div>
-            <div className="
-            grid grid-rows-[1fr,auto] 
-            gap-y-8
-            ">
-                <div className="m-auto">
-                    <ReactiveImg webSrc={destination.images.webp} fallbackSrc={destination.images.png} alt={destination.name}
-                    className="w-[150px]"
-                     />
-                </div>
-                <div className="flex flex-col gap-y-6">
-                  <div>
-                      <ul className="flex flex-row gap-x-8 justify-center">
-                          {
-                              destinations.map((item) => {
-                                  return (
-                                      <li key={item.name}>
-                                          <Tab label={item.name} handleClick={() => handleTab(item.name)} 
-                                          active={destination.name === item.name}
-                                              />
-                                      </li>
-                                  )
-                              })
-                          }
-                      </ul>
-                  </div>
-                  <div className="text-center">
-                      <h1 className="text-white text-[56px] font-bellefair uppercase">{destination.name}</h1>
-                      <p className="text-blue-300 text-[15px] leading-[1.8] font-barlow">{destination.description}</p>
-                  </div>
-                  <Line />
-                  <div className="flex flex-col gap-y-6 justify-center">
-                    <Statistic title="avg. distance" value={destination.distance} />
-                    <Statistic title="est. travel time" value={destination.travel} />
-                  </div>
-                </div>
+            <div className="text-center">
+                <h1 className="text-white text-[56px] font-bellefair uppercase">{destination.name}</h1>
+                <p className="text-blue-300 text-[15px] leading-[1.8] font-barlow">{destination.description}</p>
             </div>
-        </section>
+            <Line />
+            <div className="flex flex-col gap-y-6 justify-center">
+              <Statistic title="avg. distance" value={destination.distance} />
+              <Statistic title="est. travel time" value={destination.travel} />
+            </div>
+          </div>
+            
+        }
+
+        image={
+          <div className="m-auto">
+              <ReactiveImg webSrc={destination.images.webp} fallbackSrc={destination.images.png} alt={destination.name}
+              className="w-[150px]"
+               />
+          </div>
+        }
+        />
     )
 }
 
