63596f9104f355a44fa7b69103553fa6a4f85ec9
garyeung <5206130+garyeung@users.noreply.github.com>
2026-01-19 10:16:42 +0800

    fetching data and import Destionation component

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,25 @@ import './App.css'
 import Root from '@/components/combinations/Root';
 import Home from '@/pages/Home';
 import { appConfig } from '@/app.config';
+import Destination from '@/pages/Destination';
+import { fetchData, IData } from '@/services/fetchData';
+import { useEffect, useState } from 'react';
 
 function App() {
-    
+  const [data, setData] = useState<IData|null>(null);
+
+  useEffect(() => {
+    const fetching = async () => {
+      const response = await fetchData()
+      setData(response);
+    }
+    fetching();
+  }, [])
+
+  if (!data) {
+    return <div>Loading...</div>
+  }
+  
   const router = createBrowserRouter([
     {
       path: '/',
@@ -15,6 +31,10 @@ function App() {
           index: true,
           element: <Home title={appConfig.home.title} subTitle={appConfig.home.subtitle} description={appConfig.home.description}/>,
         },
+        {
+          path: appConfig.destination.path,
+          element: <Destination destinations={data.destinations} /> 
+        }
       ]
     }
   ])
