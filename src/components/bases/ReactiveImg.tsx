4e5da2a43bede63562f40189b885a1f3c05672c5
garyeung <5206130+garyeung@users.noreply.github.com>
2026-01-20 17:20:59 +0800

    adjust style

--- src/components/bases/ReactiveImg.tsx
+++ src/components/bases/ReactiveImg.tsx
@@ -22,7 +22,7 @@ const ReactiveImg = ({webSrc, fallbackSrc, alt,className}:Props) => {
     },[])
 
     return (
-    <div className={`relative w-full h-full flex justify-center items-center`}>
+    <div className={`w-full h-full flex justify-center items-center`}>
       {loading&&<div className=" w-12 h-12 border-4 border-white/50 rounded-full animate-spin bg-white"></div>}
 
       <picture>
