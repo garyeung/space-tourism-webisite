5668230b19a7098e6f8d3880cc795a9a9580e923
garyeung <5206130+garyeung@users.noreply.github.com>
2026-01-20 17:22:26 +0800

    adjust props

--- src/components/combinations/NavLinks.tsx
+++ src/components/combinations/NavLinks.tsx
@@ -3,15 +3,18 @@ import Nav from "../bases/Nav";
 
 const NavLinks = () => {
     return (
-        <ul className="flex flex-col md:flex-row md:gap-x-12 gap-y-8">
+        <ul className="flex flex-col md:flex-row md:gap-x-12 gap-y-8 md:justify-end
+        ">
             {
                 routes.map((route,index) => {
 
-                const number = index.toString().padStart(2, "0");
-                const label = number + " " +route.name;
+                const number = index;
+                const label = route.name;
                 return (
                     <li key={route.path}>
-                        <Nav path={route.path} label={label} />
+                        <Nav path={route.path} label={label} 
+                        number={number}
+                        />
                     </li>
                 )
                
