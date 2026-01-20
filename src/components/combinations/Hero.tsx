36436e22d182c7f35a96e0006970d6c582ff1254
garyeung <5206130+garyeung@users.noreply.github.com>
2026-01-20 17:21:34 +0800

    adjust style

--- src/components/combinations/Hero.tsx
+++ src/components/combinations/Hero.tsx
@@ -7,23 +7,46 @@ interface HeroProps {
 const Hero = (props:HeroProps) => {
 
     return (
-        <section className="grid grid-rows-[auto,1fr]">
-            <article className="text-blue-300 text-center">
-              <p className="font-barlow text-base uppercase tracking-[15%]">{props.subtitle}</p>
+        <section className="
+        grid 
+        grid-rows-[auto,1fr]
+        lg:grid-cols-2
+        lg:grid-rows-[1fr,auto]
+        items-center
+        justify-items-center
+        md:items-end
+        lg:items-center
+        lg:justify-items-end  
+        ">
+            <article className="
+            text-blue-300 
+            text-center
+            flex flex-col gap-y-6
+            max-w-lg
+            lg:max-w-[540px]
+            m-auto
+            lg:mb-0
+            lg:text-left
+            ">
+              <p className="
+              font-barlow text-base uppercase tracking-[15%]
+              md:text-[28px]
+              md:tracking-[4px]
+              ">{props.subtitle}</p>
               <h1 className="
                 font-bellefair
               text-white
                 text-[80px]
                 uppercase
+                md:text-[144px]
               ">{props.title}</h1>
               <p className="
                 font-barlow
-                text-[15px]
-                leading-[1.8]
+                description 
               "
               >{props.descrition}</p>
             </article>
-            <div className="m-auto">
+            <div>
                 {props.children}
             </div>
         </section>
