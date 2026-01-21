ef62821584a8ab524604b5707b1635efb238e0d5
garyeung <5206130+garyeung@users.noreply.github.com>
2026-01-22 00:29:14 +0800

    import spinner and replace useEffect with useLayoutEffect and set laoding each props changed

--- src/components/bases/ReactiveImg.tsx
+++ src/components/bases/ReactiveImg.tsx
@@ -1,31 +1,37 @@
-import { useEffect, useRef, useState } from "react"
+import { useLayoutEffect, useRef, useState } from "react";
+import Spinner from "@/components/bases/Spinner";
 
 interface Props {
     webSrc: string,
     fallbackSrc: string,
     alt: string,
-    className?: string
+    className?: string,
+    lazy?: boolean
 }
 
-const ReactiveImg = ({webSrc, fallbackSrc, alt,className}:Props) => {
+const ReactiveImg = ({webSrc, fallbackSrc, alt, className, lazy = false}:Props) => {
     const [loading, setLoading] = useState(true);
     const imgRef = useRef<HTMLImageElement>(null);
 
     const handleLoad = () => {
         setLoading(false);
     }
-   
-    useEffect(()=> {
-        if(imgRef.current?.complete) {
-            handleLoad();
-        }
-    },[])
 
+    useLayoutEffect(() => {
+        if (imgRef.current?.complete) {
+            setLoading(false);
+        } else {
+            setLoading(true);
+        }
+    }, [webSrc, fallbackSrc])
+   
     return (
     <div className={`relative w-full h-full flex justify-center items-center`}>
-      {loading&&<div className=" absolute w-full h-full grid justify-center items-center">
-        <svg className="mr-3 -ml-1 size-5 animate-spin text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle><path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path></svg>
-      </div>}
+      {loading && 
+      <div className="absolute w-full h-full grid justify-center items-center">
+        <Spinner />
+      </div>
+      }
 
       <picture>
         <source srcSet={webSrc} type="image/webp" />
@@ -33,7 +39,8 @@ const ReactiveImg = ({webSrc, fallbackSrc, alt,className}:Props) => {
         ref={imgRef}
         src={fallbackSrc} alt={alt} 
         onLoad={handleLoad}
-        loading="eager"
+        onError={handleLoad}
+        loading={lazy? "lazy": "eager"}
         className={`
           ${loading? "opacity-0": "opacity-100"}
           object-cover
