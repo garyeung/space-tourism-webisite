1c76a762048986fbcbf522111912a50977d403bc
garyeung <5206130+garyeung@users.noreply.github.com>
2026-01-21 22:39:05 +0800

    adjust syle and add className for props

--- src/components/bases/PageLayout.tsx
+++ src/components/bases/PageLayout.tsx
@@ -3,40 +3,43 @@ interface Props {
     title: string,
     image: React.ReactNode,
     explantion: React.ReactNode
+    contentStyle?: string,
     className?: string
 }
 
-const PageLayout = ({number, title, image, explantion, className: contentStyle}:Props) => {
+const PageLayout = ({number, title, image, explantion, contentStyle, className}:Props) => {
     return (
-        <div
-        className="
+        <main
+        className={`
         p-6 
         flex-1 
         grid 
-        md:px-10 
-        md:py-32
+        md:p-10 
         lg:px-0
-        lg:py-32
-        justify-items-center
-        lg:items-end
-        "
+        lg:py-12
+        ${className||""}`}
         >
-          <section className="flex flex-col gap-y-6">
+          <section className="flex flex-col gap-y-6 justify-between">
               <div>
                   <h2 className="font-barlow text-base
                   text-center
                   uppercase
                   tracking-[15%]
-                 text-white"
-                  ><span className="opacity-25 pr-6">{number.toString().padStart(2,'0')}</span> {title} 
+                  lg:tracking-[4px]
+                 text-white
+                 md:text-left
+                 md:text-xl
+                 lg:text-[28px]
+                 "
+                  ><span className="opacity-25 pr-6 font-bold lg:tracking-[4.72px]">{number.toString().padStart(2,'0')}</span>{title} 
                   </h2>
               </div>
-              <div className={`flex flex-col gap-y-8 ${contentStyle || ''}`}>
+              <div className={`grid grid-rows-[auto,1fr] justify-items-center gap-8 md:grid-rows-2 lg:grid-rows-none lg:grid-cols-2 ${contentStyle || ''}`}>
                   {image}
                   {explantion}
               </div>
           </section>
-        </div>
+        </main>
     )
 }
 
