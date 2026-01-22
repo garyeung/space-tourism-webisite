064b8cc050bb96448c5be89abe96a573114e4d9c
garyeung <5206130+garyeung@users.noreply.github.com>
2026-01-22 11:07:33 +0800

    update loading page

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Root from '@/pages/Root';
 import Home from '@/pages/Home';
 import Crew from '@/pages/Crew';
 import Technology from '@/pages/Technology';
+import Spinner from './components/bases/Spinner';
 
 function App() {
   const [data, setData] = useState<IData|null>(null);
@@ -20,7 +21,12 @@ function App() {
   }, [])
 
   if (!data) {
-    return; 
+    return ( 
+      <div className='absolute w-full h-full inset-0 flex items-center justify-center bg-white/10'>
+        <Spinner light={false}/>
+        loading...
+      </div>
+    );
   }
   
   const router = createBrowserRouter([
