a3c4d0070b49d452377033159d7bded392437348
garyeung <5206130+garyeung@users.noreply.github.com>
2026-01-22 10:49:31 +0800

    rm css file

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import './App.css'
 import { appConfig } from '@/app.config';
 import Destination from '@/pages/Destination';
 import { fetchData, IData } from '@/services/fetchData';
